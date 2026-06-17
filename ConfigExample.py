5b76ff2fab881dbbbff8cb9cc39a953d6ff78a08
koste <kostenkodi2006@gmail.com>
2026-06-17 21:32:55 +0600

    Добавлен пример конфига

---
+++ ConfigExample.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+TOKEN = ""
+DB_CONFIG = ""
+
+# Rule34
+R34_USER_ID = ""
+R34_API_KEY = ""
+MAX_DAILY_LIMIT = 
+
+# Погодка
+WEATHER_API_KEY = ""
+
+# Настройки для медицинского модуля
+MEDIC_USER_ID =   # ID медика
+OWNER_USER_ID =   # ID для тестов
+ALLOWED_MEDICS = [MEDIC_USER_ID]
